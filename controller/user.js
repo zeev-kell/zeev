5e352eb9615b2e7624533a8a0625b63c09a2bf15
zeev <zeevkell@gmail.com>
2016-10-16 18:16:43 +0800

    修改admin权限

--- controller/user.js
+++ controller/user.js
@@ -6,43 +6,43 @@ var debug = require('debug')('zeev:user');
 var handleError = require("../utils").handleError;
 var User = global.dbHelper.getModel("User");
 
-exports.adminRequired = function(req, res, next) {
-    //	var user = req.session || req.session.user;
-    var user = null,
-        method = req.method;
-    if (user) {
-        debug("user", req.method);
-        if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
-            return res.status(403).send({ msg: '用户权限不够' });
-        }
-        return res.redirect(303, '/signin');
-    }
-    next();
+exports.adminRequired = function (req, res, next) {
+	// var user = req.session || req.session.user,
+	var user = null,
+		method = req.method;
+	if (!user) {
+		debug("user", req.method);
+		if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
+			return res.status(403).send({ msg: '用户权限不够' });
+		}
+		return res.redirect(303, '/signin');
+	}
+	next();
 }
 
-exports.signin = function(req, res) {
-    var _user = {
-        name: req.body.name,
-        password: req.body.password
-    }
-    debug("signin", _user);
-    User.findOne({ name: _user.name })
-        .then(function(user) {
-            if (user) {
-                user.compare(_user, function(err, isMacth) {
-                    if (isMacth && user.role >= 10) {
-                        // req.session.user = user;
-                        res.cookie('active', 'true');
-                        res.cookie('name', user.name);
-                        res.cookie('role', user.role);
-                        res.sendStatus(200);
-                    } else {
-                        res.sendStatus(403);
-                    }
-                })
-            } else {
-                res.sendStatus(403);
-            }
+exports.signin = function (req, res) {
+	var _user = {
+		name    : req.body.name,
+		password: req.body.password
+	}
+	debug("signin", _user);
+	User.findOne({ name: _user.name })
+		.then(function (user) {
+			if (user) {
+				user.compare(_user, function (err, isMacth) {
+					if (isMacth && user.role >= 10) {
+						// req.session.user = user;
+						res.cookie('active', 'true');
+						res.cookie('name', user.name);
+						res.cookie('role', user.role);
+						res.sendStatus(200);
+					} else {
+						res.sendStatus(403);
+					}
+				})
+			} else {
+				res.sendStatus(403);
+			}
 
-        })
+		})
 }
