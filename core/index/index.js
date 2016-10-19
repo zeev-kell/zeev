065026d1339aff4e0a1d9e32696f9267bbb27a25
zeev <zeev>
2016-10-19 09:15:47 +0800

    change theme

--- core/index/index.js
+++ core/index/index.js
@@ -1,7 +1,15 @@
 'use strict';
-window.onload = function () {
-	var textArray = ["Front End Developer", "Front End Developer", "Front End Developer", "Front End Developer"];
-	new TxtRotate(document.getElementById("keyword"), textArray, 1000);
-	cvsEffect.one();
-	cvsEffect.start()
+window.onload = function() {
+    var textArray = ["Front End Developer", "Front End Developer", "Front End Developer", "Front End Developer"];
+    new TxtRotate(document.getElementById("keyword"), textArray, 1000);
+    cvsEffect.one();
+    cvsEffect.start()
 }
+
+$(function() {
+	$("#nav li").mouseenter(function(event){
+		event.preventDefault();
+		event.stopPropagation();
+		console.log(this);
+	})
+})
