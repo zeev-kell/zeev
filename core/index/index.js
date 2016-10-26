b3c037226f7bcb94f48104e44d4ca403faa2fdd1
zeev <zeevkell@gmail.com>
2016-10-26 19:58:26 +0800

    remove some js

--- core/index/index.js
+++ core/index/index.js
@@ -1,17 +1,17 @@
 'use strict';
-window.onload = function() {
-    var textArray = ["Front End Developer", "Front End Developer", "Front End Developer", "Front End Developer"];
-    new TxtRotate(document.getElementById("keyword"), textArray, 1000);
-    Effect.one();
-    Effect.start();
+window.onload = function () {
+	var textArray = ["Front End Developer", "Front End Developer", "Front End Developer", "Front End Developer"];
+	new TxtRotate(document.getElementById("keyword"), textArray, 1000);
+	Effect.one();
+	Effect.start();
 }
 
-$(function() {
-    $("#nav li").mouseenter(function(event) {
-        var _this = $(this);
-        event.preventDefault();
-        event.stopPropagation();
-        // var index = Number(_this.attr("index"));
-        // Effect.change(index);
-    })
+$(function () {
+	//    $("#nav li").mouseenter(function(event) {
+	//        var _this = $(this);
+	//        event.preventDefault();
+	//        event.stopPropagation();
+	// var index = Number(_this.attr("index"));
+	// Effect.change(index);
+	//    })
 })
