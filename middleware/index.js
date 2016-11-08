de19a2a6e883f2caaa4be90dab985525747a47e2
zeev <zeevkell@gmail.com>
2016-11-08 15:46:37 +0800

    merge comment

--- middleware/index.js
+++ middleware/index.js
@@ -2,18 +2,19 @@
  * Created by zeev on 2016/11/6 0006.
  */
 var debug  = require('debug')('zeev:middleware'),
-    _      = require('lodash'),
-    errors = require("../errors");
+	_      = require('lodash'),
+	errors = require("../errors");
+
 exports.authenticateClient = function (req, res, next) {
-    var session = req.session;
-    var method = req.method;
-    if (!session.user) {
-        debug("no signin user", req.method);
-        if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
-            return errors.handleAPIError(new errors.UnauthorizedError('未登录...'), req, res, next);
-        }
-        return res.redirect(303, '/signin');
-    }
-    debug("signin user : ", session.user.name ||　session.user　|| session);
-    next();
+	var session = req.session;
+	var method  = req.method;
+	if (!session.user) {
+		debug("no signin user", req.method);
+		if (method === 'POST' || method === 'PUT' || method === 'DELETE') {
+			return errors.handleAPIError(new errors.UnauthorizedError('未登录...'), req, res, next);
+		}
+		return res.redirect(303, '/signin');
+	}
+	//debug("signin user : ", session.user.name ||　session.user　|| session);
+	next();
 }
