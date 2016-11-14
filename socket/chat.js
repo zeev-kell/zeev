76148ad3f25d143081d94cd312445617f1a08317
zeev <zeev>
2016-11-14 11:37:15 +0800

    chat

--- socket/chat.js
+++ socket/chat.js
@@ -2,15 +2,19 @@
  * Created by zeev on 2016/11/11 0011.
  */
 
-module.exports = function (chat) {
-	chat.on('connection', function (socket) {
-			socket.emit('news', { message: 'chat -- hello world !' });
-		});
-	return chat
+module.exports = function(chat) {
+    chat.on('connection', function(socket) {
+        socket.emit('new', { message: 'hello world !', date: new Date() });
+        setInterval(function() {
+            socket.emit('chat', { message: 'chat -- hello world !', date: new Date() });
+        }, 1000);
+        socket.emit('chat', { message: 'chat -- hello world !', date: new Date() });
+    });
+    return chat
 }
 
 /* ¹ã²¥ÊÂ¼þ */
-module.exports.emit = function () {
-	stream.write(arguments[0] + JSON.stringify(arguments) + '\n');
-	return io.sockets.emit.apply(io.sockets, arguments);
+module.exports.emit = function() {
+    stream.write(arguments[0] + JSON.stringify(arguments) + '\n');
+    return io.sockets.emit.apply(io.sockets, arguments);
 }
