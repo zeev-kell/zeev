198bd63c389001f24f4e82048533212a960da5b7
zeev <zeevkell@gmail.com>
2016-11-28 15:32:00 +0800

    add product start

--- core/home/index.js
+++ core/home/index.js
@@ -2,16 +2,7 @@
 window.onload = function () {
 	var textArray = ["Front End Developer", "Front End Developer", "Front End Developer", "Front End Developer"];
 	new TxtRotate(document.getElementById("keyword"), textArray, 1000);
-	Effect.one();
-	Effect.start();
+	var start = window.start = new Start({
+		canvas: document.getElementById("effect")
+	});
 }
-
-//$(function () {
-	//    $("#nav li").mouseenter(function(event) {
-	//        var _this = $(this);
-	//        event.preventDefault();
-	//        event.stopPropagation();
-	// var index = Number(_this.attr("index"));
-	// Effect.change(index);
-	//    })
-//})
